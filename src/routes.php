c6f2de2750fb8a5ae03751b155a24d9445482f74
Adnan Kičin <adnankicin92@gmail.com>
2017-08-28 16:42:12 +0200

    crud methods

--- src/routes.php
+++ src/routes.php
@@ -15,3 +15,35 @@ $app->get('/api/pjesme/[{id}]', function($request, $response, $args){
     $pjesme = $query->fetchObject();
     return $this->response->withJson($pjesme);
 });
+
+
+$app->post('/api/pjesme', function ($request, $response) {
+    $input = $request->getParsedBody();
+    $sql = "INSERT INTO pjesme (url) VALUES (:url)";
+    $query = $this->db->prepare($sql);
+    $query->bindParam("url", $input['url']);
+    //add other
+    $query->execute();
+    $input['id'] = $this->db->lastInsertId();
+    return $this->response->withJson($input);
+});
+    
+
+$app->delete('/api/pjesme/[{id}]', function ($request, $response, $args) {
+    $query = $this->db->prepare("DELETE FROM pjesme WHERE id=:id");
+    $query->bindParam("id", $args['id']);
+    $query->execute();
+    $pjesme = $query->fetchAll();
+    return $this->response->withJson($pjesme);
+});
+
+$app->put('/api/pjesme/[{id}]', function ($request, $response, $args) {
+    $input = $request->getParsedBody();
+    $sql = "UPDATE pjesme SET url=:url WHERE id=:id";
+    $query = $this->db->prepare($sql);
+    $query->bindParam("id", $args['id']);
+    $query->bindParam("url", $input['url']);
+    $query->execute();
+    $input['id'] = $args['id'];
+    return $this->response->withJson($input);
+});
